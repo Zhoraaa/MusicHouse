ffbe5636a2dff0011bb73c6eb3113f035f37cd9c
zh0raaa <102014678+Zhoraaa@users.noreply.github.com>
2023-06-29 18:16:07 +0500

    sada

--- music_house.sql
+++ music_house.sql
@@ -3,122 +3,332 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Май 07 2023 г., 18:39
+-- Время создания: Июн 29 2023 г., 15:18
 -- Версия сервера: 8.0.30
--- Версия PHP: 7.2.34
-SET
-  SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+-- Версия PHP: 8.1.9
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
+SET time_zone = "+00:00";
 
-SET
-  time_zone = "+00:00";
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
-;
-
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */
-;
-
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */
-;
-
-/*!40101 SET NAMES utf8mb4 */
-;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- База данных: `music_house`
 --
+
 -- --------------------------------------------------------
+
+--
+-- Структура таблицы `cancelled_orders`
+--
+
+CREATE TABLE `cancelled_orders` (
+  `id` int NOT NULL,
+  `order` int NOT NULL,
+  `reason` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `countries`
+--
+
+CREATE TABLE `countries` (
+  `id` int NOT NULL,
+  `name` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `countries`
+--
+
+INSERT INTO `countries` (`id`, `name`) VALUES
+(1, 'Франция'),
+(2, 'Россия'),
+(3, 'Япония'),
+(4, 'Германия'),
+(5, 'США'),
+(6, 'Швеция'),
+(7, 'Китай');
+
+-- --------------------------------------------------------
+
 --
 -- Структура таблицы `orders`
 --
+
 CREATE TABLE `orders` (
   `id` int NOT NULL,
-  `customer` int NOT NULL,
-  `name` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` json NOT NULL,
-  `created_at` date NOT NULL,
-  `type` int NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+  `client` int NOT NULL,
+  `product` int NOT NULL,
+  `status` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
+
 --
 -- Структура таблицы `products`
 --
+
 CREATE TABLE `products` (
   `id` int NOT NULL,
-  `name` varchar(128) NOT NULL,
-  `description` varchar(128) NOT NULL,
+  `name` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
   `cost` int NOT NULL,
-  `count` int NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+  `country` int NOT NULL,
+  `create_date` date NOT NULL,
+  `type` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `image`, `cost`, `country`, `create_date`, `type`) VALUES
+(1, 'Скрипка', '0e4fdd6e73b70eb36ff8680be51b06c8.jpeg', 1699, 1, '2004-06-26', 1),
+(2, 'Бит-машина', '3baa734380623915c1cdcb21d324605d.jpeg', 7000, 4, '2023-05-30', 7),
+(3, 'Синтезатор', 'e88528d4f986c590356a00afcfa1dc1f.png', 50000, 2, '2023-06-02', 7),
+(4, 'Электро-гитара', '9c8d5686a6ab6584bd8472f2ede969d4.jpeg', 30000, 3, '2022-03-30', 1),
+(5, 'Гитара', '467e46ee26a3161b5cd8982c7e0891b0.jpeg', 7000, 7, '2015-02-28', 1),
+(6, 'Скрипка Ханз Зиммера', '115707438fcf8b95a079526f808bc9ad.jpeg', 100000, 5, '2004-05-29', 1);
 
 -- --------------------------------------------------------
+
+--
+-- Структура таблицы `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int NOT NULL,
+  `name` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `roles`
+--
+
+INSERT INTO `roles` (`id`, `name`) VALUES
+(1, 'Админ'),
+(2, 'Клиент');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `statuses`
+--
+
+CREATE TABLE `statuses` (
+  `id` int NOT NULL,
+  `name` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `statuses`
+--
+
+INSERT INTO `statuses` (`id`, `name`) VALUES
+(1, 'Новый'),
+(2, 'Старый'),
+(3, 'Отказ');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `types`
+--
+
+CREATE TABLE `types` (
+  `id` int NOT NULL,
+  `name` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `types`
+--
+
+INSERT INTO `types` (`id`, `name`) VALUES
+(1, 'Струнные'),
+(2, 'Ударные'),
+(3, 'Смычковые'),
+(4, 'Бассовые'),
+(5, 'Электро'),
+(6, 'Духовые'),
+(7, 'Клавишные');
+
+-- --------------------------------------------------------
+
 --
 -- Структура таблицы `users`
 --
+
 CREATE TABLE `users` (
   `id` int NOT NULL,
-  `surname` varchar(128) NOT NULL,
-  `name` varchar(128) NOT NULL,
-  `patronymic` varchar(128) DEFAULT NULL,
-  `login` varchar(128) NOT NULL,
-  `email` varchar(128) NOT NULL,
-  `password` varchar(128) NOT NULL,
-  `basket` varchar(1024) NOT NULL,
+  `surname` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `patronymic` varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `login` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
   `role` int NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `surname`, `name`, `patronymic`, `login`, `email`, `password`, `role`) VALUES
+(1, 'Админов', 'Админ', 'Админович', 'testAcc', 'test@acc.com', 'efe6398127928f1b2e9ef3207fb82663', 1),
+(2, 'Админов', 'Админ', 'Админович', 'admin', 'admin111@gmail.com', '1341215dbe9acab4361fd6417b2b11bc', 1);
 
 --
 -- Индексы сохранённых таблиц
 --
+
+--
+-- Индексы таблицы `cancelled_orders`
+--
+ALTER TABLE `cancelled_orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `order` (`order`,`reason`);
+
+--
+-- Индексы таблицы `countries`
+--
+ALTER TABLE `countries`
+  ADD PRIMARY KEY (`id`);
+
 --
 -- Индексы таблицы `orders`
 --
-ALTER TABLE
-  `orders`
-ADD
-  PRIMARY KEY (`id`),
-ADD
-  KEY `customer` (`customer`, `type`);
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `customer` (`client`,`status`),
+  ADD KEY `product` (`product`),
+  ADD KEY `status` (`status`);
+
+--
+-- Индексы таблицы `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `country` (`country`,`type`),
+  ADD KEY `type` (`type`);
+
+--
+-- Индексы таблицы `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `statuses`
+--
+ALTER TABLE `statuses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `types`
+--
+ALTER TABLE `types`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `users`
 --
-ALTER TABLE
-  `users`
-ADD
-  PRIMARY KEY (`id`),
-ADD
-  KEY `role` (`role`);
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `role` (`role`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
 --
+
+--
+-- AUTO_INCREMENT для таблицы `cancelled_orders`
+--
+ALTER TABLE `cancelled_orders`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `countries`
+--
+ALTER TABLE `countries`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
 --
 -- AUTO_INCREMENT для таблицы `orders`
 --
-ALTER TABLE
-  `orders`
-MODIFY
-  `id` int NOT NULL AUTO_INCREMENT;
+ALTER TABLE `orders`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблицы `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT для таблицы `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `statuses`
+--
+ALTER TABLE `statuses`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблицы `types`
+--
+ALTER TABLE `types`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
-ALTER TABLE
-  `users`
-MODIFY
-  `id` int NOT NULL AUTO_INCREMENT;
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `cancelled_orders`
+--
+ALTER TABLE `cancelled_orders`
+  ADD CONSTRAINT `cancelled_orders_ibfk_1` FOREIGN KEY (`order`) REFERENCES `orders` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Ограничения внешнего ключа таблицы `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`client`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`product`) REFERENCES `products` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `orders_ibfk_3` FOREIGN KEY (`status`) REFERENCES `statuses` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Ограничения внешнего ключа таблицы `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`country`) REFERENCES `countries` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`type`) REFERENCES `types` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Ограничения внешнего ключа таблицы `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role`) REFERENCES `roles` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
-;
-
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */
-;
-
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */
-;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
